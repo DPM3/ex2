6c181557778299edbd20b44bc08446e3530cb42c
DPM3 <dpm3git@gmail.com>
2020-08-16 19:05:41 +0300

    finished ver.1 of BMPImg header

--- src/BMPImg.h
+++ src/BMPImg.h
@@ -7,9 +7,12 @@
 #include"Color.h"
 
 class BMPImg {
+	unsigned int width, height;
+	Color* data;
 public:
 	BMPImg() = default;
 	BMPImg(std::string const& path);
 	void load(std::string const& path);
 	void save(std::string const& path);
-}
+	Color& operator() (int i, int j);
+};

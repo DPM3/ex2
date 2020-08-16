1f36eba22ef0ae250e2d368d023c229b0351e84a
DPM3 <dpm3git@gmail.com>
2020-08-16 19:40:34 +0300

    added myBmpImg.width() and .height()

--- src/BMPImg.h
+++ src/BMPImg.h
@@ -5,13 +5,15 @@
 #include"Color.h"
 
 class BMPImg {
-	unsigned int m_width, m_height;
+	int m_width, m_height;
 	Color* m_data;
 public:
 	BMPImg() = default;
-	BMPImg(unsigned int width, unsigned int height);
+	BMPImg(int width, int height);
 	BMPImg(std::string const& path);
 	void load(std::string const& path);
 	void save(std::string const& path);
 	Color& operator() (int i, int j);
+	int width();
+	int height();
 };

a219894f032668a3d872272e26b8c78ad0a47b93
DPM3 <dpm3git@gmail.com>
2020-08-17 14:50:17 +0300

    implementation of BMPImg start

---
+++ src/BMPImg.cpp
@@ -0,0 +1,50 @@
+#include"BMPImg.h"
+#include<fstream>
+
+BMPImg::BMPImg(std::string const& path) : BMPImg{} {
+	load(path);
+}
+
+BMPImg::BMPImg(int width, int height) 
+	: m_width{width}, m_height{height}, m_data{new Color[width * height]} { }
+
+void load(std::string const& path) {
+	std::ifstream file {path};
+	if (!file) {
+		//TODO: throw something here.
+	}
+
+	//after these 3 lines, s.c_str() brings an array identical to the file,
+	//and contentp is a pointer moving around that array.
+	std::string s;
+	file >> s;
+	char* contentp = s.c_str();
+
+	//this function reads the char pointed by contentp, and increments contentp to the next char.
+	auto readChar = [](char* *contentp) -> char {
+		return *( (*contentp)++ );
+	};
+	//this function reads the int pointed by contentp, and increments contentp to the next int.
+	auto readInt = [](char* *contentp) -> int {
+		int& result = *( (int*) *contentp );
+		*contentp += sizeof(int) / sizeof(char);
+		return result;
+	};
+
+	//////////-- The Header --//////////
+	if (readChar(&contentp) != 'B' || readChar(&contentp) != 'M') {
+		//TODO: throw something here.
+	}
+	int fileSize = readInt(&contentp);//get file size(array size also)
+	contentp += 4; //move past the 4 bytes of reserved content
+	int arrayOffset = readInt(&contentp);//get pixel array offset in array
+
+	//////////-- The DIB Header --//////////
+	
+}
+
+void save(std::string const& path);
+
+Color& operator() (int i, int j) {
+	return data[width * i + j];
+}

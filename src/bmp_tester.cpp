ab667b819ade61d4b0ba34fd33997109e9eed045
DPM3 <dpm3git@gmail.com>
2020-08-19 19:31:58 +0300

    fixed eye catching bugs in bmp_tester.cpp

--- src/bmp_tester.cpp
+++ src/bmp_tester.cpp
@@ -9,30 +9,31 @@ void rotate_image(const std::string& imagePath, const std::string& outputPath) {
 	BMPImg iimg = BMPImg(imagePath);
 
 	//The width and height of the input image, or the height and width of the output image
-	const int iwidth_oheight = oimg.width();
-	const int iheight_owidth = oimg.height();
+	const int iwidth_oheight = iimg.width();
+	const int iheight_owidth = iimg.height();
 
 	BMPImg oimg = BMPImg(iheight_owidth, iwidth_oheight);
 
 	//First we'll transpose the image
-	for (int i = 0; i < iheight_owidth; i++) {
-		for (int j = 0; j < iwidth_oheight; j++) {
+	for (int i = 0; i < iwidth_oheight; i++) {
+		for (int j = 0; j < iheight_owidth; j++) {
 			oimg(j, i) = iimg(i, j);
 		}
 	}
 
-	//Then we'll reverse each row of the output image, and that'll make a 90 degress rotation
+	//Then we'll reverse each row of the output image, and that'll make a 90 degrees rotation
 	for (int i = 0; i < iwidth_oheight; i++) {
 		for (int j = 0; j < iheight_owidth / 2; j++) {
-			oimg(i, j) = oimg(i, iheight_owidth - j);
+			oimg(i, j) = oimg(i, iheight_owidth - j - 1);
 		}
 	}
+	oimg.save(outputPath);
 }
 
 void convert_to_grayscale(const std::string& imagePath,
 	const std::string& outputPath) {
 	BMPImg iimg = BMPImg(imagePath);
-	BMPImg oimg = iimg.save(iimg.getFile());
+	BMPImg oimg {iimg.width(), iimg.height()};
 	const int width = oimg.width();
 	const int height = oimg.height();
 
@@ -41,11 +42,12 @@ void convert_to_grayscale(const std::string& imagePath,
 	for (int i = 0; i < height; i++) {
 		for (int j = 0; j < width; j++) {
 			Color ic = oimg(i, j);
-			component_t b = c.brightness();
+			Color::component_t b = c.brightness();
 			Color oc = Color(b, b, b);
 			oimg(i, j) = oc;
 		}
 	}
+	oimg.save(outputPath);
 }
 }
 }
